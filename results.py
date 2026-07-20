bbe4d2bb2941ff41483134f13d53e12203fe6af2
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2026-07-20 11:57:45 +0530

    ..

--- results.py
+++ results.py
@@ -129,6 +129,6 @@ if __name__ == "__main__":
     # fetcher.fetch_round(2025, 19)
     # fetcher.fetch_round(2025, 20)
     # fetcher.fetch_round(2026, 6)
-    fetcher.fetch_round(2026, 9)
+    fetcher.fetch_round(2026, 10)
 
     logger.info("Race results fetching completed")
